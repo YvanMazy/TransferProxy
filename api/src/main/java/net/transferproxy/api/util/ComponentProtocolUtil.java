c06c39a7eb2390eab154e08f07c2bf39dd7df256
YvanMazy <mazy.yvan@protonmail.com>
2025-05-07 16:21:28 +0200

    :arrow_up: Upgrade kyori adventure dependency

--- api/src/main/java/net/transferproxy/api/util/ComponentProtocolUtil.java
+++ api/src/main/java/net/transferproxy/api/util/ComponentProtocolUtil.java
@@ -27,20 +27,21 @@ package net.transferproxy.api.util;
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
 import net.kyori.adventure.text.serializer.json.JSONOptions;
 import net.kyori.adventure.text.serializer.json.legacyimpl.NBTLegacyHoverEventSerializer;
-import net.kyori.option.OptionState;
+import net.kyori.option.OptionSchema;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
-// Credit: https://github.com/PaperMC/Velocity/blob/371e68607695b48117e1cd57d19c67e40384c33a/proxy/src/main/java/com/velocitypowered/proxy/protocol/ProtocolUtils.java
+// Credit: https://github.com/PaperMC/Velocity/blob/b411a0fa09f76aec062921b77642828408f0099f/proxy/src/main/java/com/velocitypowered/proxy/protocol/ProtocolUtils.java
 public final class ComponentProtocolUtil {
 
     private static final GsonComponentSerializer PRE_1_16_SERIALIZER = GsonComponentSerializer.builder()
             .downsampleColors()
             .legacyHoverEventSerializer(NBTLegacyHoverEventSerializer.get())
-            .options(OptionState.optionState()
+            .options(OptionSchema.globalSchema()
+                    .stateBuilder()
                     // before 1.16
                     .value(JSONOptions.EMIT_RGB, Boolean.FALSE)
-                    .value(JSONOptions.EMIT_HOVER_EVENT_TYPE, JSONOptions.HoverEventValueMode.LEGACY_ONLY)
+                    .value(JSONOptions.EMIT_HOVER_EVENT_TYPE, JSONOptions.HoverEventValueMode.VALUE_FIELD)
                     // before 1.20.3
                     .value(JSONOptions.EMIT_COMPACT_TEXT_COMPONENT, Boolean.FALSE)
                     .value(JSONOptions.EMIT_HOVER_SHOW_ENTITY_ID_AS_INT_ARRAY, Boolean.FALSE)
@@ -49,30 +50,50 @@ public final class ComponentProtocolUtil {
             .build();
     private static final GsonComponentSerializer PRE_1_20_3_SERIALIZER = GsonComponentSerializer.builder()
             .legacyHoverEventSerializer(NBTLegacyHoverEventSerializer.get())
-            .options(OptionState.optionState()
+            .options(OptionSchema.globalSchema()
+                    .stateBuilder()
                     // after 1.16
                     .value(JSONOptions.EMIT_RGB, Boolean.TRUE)
-                    .value(JSONOptions.EMIT_HOVER_EVENT_TYPE, JSONOptions.HoverEventValueMode.MODERN_ONLY)
+                    .value(JSONOptions.EMIT_HOVER_EVENT_TYPE, JSONOptions.HoverEventValueMode.CAMEL_CASE)
+                    .value(JSONOptions.EMIT_CLICK_EVENT_TYPE, JSONOptions.ClickEventValueMode.CAMEL_CASE)
                     // before 1.20.3
                     .value(JSONOptions.EMIT_COMPACT_TEXT_COMPONENT, Boolean.FALSE)
                     .value(JSONOptions.EMIT_HOVER_SHOW_ENTITY_ID_AS_INT_ARRAY, Boolean.FALSE)
                     .value(JSONOptions.VALIDATE_STRICT_EVENTS, Boolean.FALSE)
                     .build())
             .build();
-    private static final GsonComponentSerializer MODERN_SERIALIZER = GsonComponentSerializer.builder()
+    private static final GsonComponentSerializer PRE_1_21_5_SERIALIZER = GsonComponentSerializer.builder()
             .legacyHoverEventSerializer(NBTLegacyHoverEventSerializer.get())
-            .options(OptionState.optionState()
+            .options(OptionSchema.globalSchema()
+                    .stateBuilder()
                     // after 1.16
                     .value(JSONOptions.EMIT_RGB, Boolean.TRUE)
-                    .value(JSONOptions.EMIT_HOVER_EVENT_TYPE, JSONOptions.HoverEventValueMode.MODERN_ONLY)
+                    .value(JSONOptions.EMIT_HOVER_EVENT_TYPE, JSONOptions.HoverEventValueMode.CAMEL_CASE)
+                    .value(JSONOptions.EMIT_CLICK_EVENT_TYPE, JSONOptions.ClickEventValueMode.CAMEL_CASE)
                     // after 1.20.3
                     .value(JSONOptions.EMIT_COMPACT_TEXT_COMPONENT, Boolean.TRUE)
                     .value(JSONOptions.EMIT_HOVER_SHOW_ENTITY_ID_AS_INT_ARRAY, Boolean.TRUE)
                     .value(JSONOptions.VALIDATE_STRICT_EVENTS, Boolean.TRUE)
                     .build())
             .build();
+    private static final GsonComponentSerializer MODERN_SERIALIZER = GsonComponentSerializer.builder()
+            .legacyHoverEventSerializer(NBTLegacyHoverEventSerializer.get())
+            .options(OptionSchema.globalSchema()
+                    .stateBuilder()
+                    // after 1.16
+                    .value(JSONOptions.EMIT_RGB, Boolean.TRUE)
+                    .value(JSONOptions.EMIT_HOVER_EVENT_TYPE, JSONOptions.HoverEventValueMode.SNAKE_CASE)
+                    .value(JSONOptions.EMIT_CLICK_EVENT_TYPE, JSONOptions.ClickEventValueMode.SNAKE_CASE)
+                    // after 1.20.3
+                    .value(JSONOptions.EMIT_COMPACT_TEXT_COMPONENT, Boolean.TRUE)
+                    .value(JSONOptions.EMIT_HOVER_SHOW_ENTITY_ID_AS_INT_ARRAY, Boolean.TRUE)
+                    // after 1.21.5
+                    .value(JSONOptions.EMIT_HOVER_SHOW_ENTITY_KEY_AS_TYPE_AND_UUID_AS_ID, Boolean.FALSE)
+                    .value(JSONOptions.VALIDATE_STRICT_EVENTS, Boolean.TRUE)
+                    .build())
+            .build();
 
-    private static final int[] SERIALIZER_PROTOCOLS = {734, 735, 765};
+    private static final int[] SERIALIZER_PROTOCOLS = {734, 735, 765, 770};
 
     private ComponentProtocolUtil() throws IllegalAccessException {
         throw new IllegalAccessException("You cannot instantiate a utility class");
@@ -89,14 +110,22 @@ public final class ComponentProtocolUtil {
     }
 
     @Contract(pure = true)
+    public static @NotNull GsonComponentSerializer getPre1215Serializer() {
+        return PRE_1_21_5_SERIALIZER;
+    }
+
+    @Contract(pure = true)
     public static @NotNull GsonComponentSerializer getModernSerializer() {
         return MODERN_SERIALIZER;
     }
 
     @Contract(pure = true)
     public static @NotNull GsonComponentSerializer getSerializer(final int protocol) {
-        if (protocol >= 765) { // >= 1.20.3
+        if (protocol >= 770) { // >= 1.21.5
             return getModernSerializer();
+        }
+        if (protocol >= 765) { // >= 1.20.3
+            return getPre1215Serializer();
         }
         if (protocol >= 735) { // >= 1.16
             return getPre1203Serializer();
