885c06e0e6515e796e7e09c54baf9edf758c8067
YvanMazy <mazy.yvan@protonmail.com>
2026-01-01 14:15:13 +0100

    :page_facing_up: Update year in copyright to 2026

--- core/src/main/java/net/transferproxy/network/packet/config/serverbound/AcceptCodeOfConductPacket.java
+++ core/src/main/java/net/transferproxy/network/packet/config/serverbound/AcceptCodeOfConductPacket.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2025 Yvan Mazy
+ * Copyright (c) 2026 Yvan Mazy
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
